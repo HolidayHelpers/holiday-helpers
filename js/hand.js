ddd2c6db2b12bed70304574d78ff4c0a52d19047
EvaGraceSmith <smithevagrace@gmail.com>
2022-12-13 13:52:55 -0600

    added js files and notes, moved buttons to top

---
+++ js/hand.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log ('Hello from the hand js file');
+
+
+//we need our lend a hand images here, with an event listener to 
+// respond to a click of the Image.
+//This is similar to what we did in the Odd Duck Project
+
+// We need a count tally on the <section class="navCartContainer">
+// divs Gifts and Acts.
+//(This is similar to the count down button on Eva's Odd Duck, but instead, it is counting up)
