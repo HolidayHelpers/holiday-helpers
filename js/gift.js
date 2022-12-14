075366af3bdbb8550a53b9d61fb4e94244833ef7
Beth Rissman <beth.rissman@telepham.com>
2022-12-13 20:48:41 -0600

    added gift array, initial thoughts on click event

--- js/gift.js
+++ js/gift.js
@@ -14,23 +14,43 @@ console.log ('Hello from the gift js file');
 
 let giftClicks = 0;
 
-let giftImage1 = document.querySelector('.giftImage1 img');
-let giftImage2 = document.querySelector('.giftImage2 img');
-let giftImage3 = document.querySelector('.giftImage3 img');
-let giftImage4 = document.querySelector('.giftImage4 img');
-let giftImage5 = document.querySelector('.giftImage5 img');
-let giftImage6 = document.querySelector('.giftImage6 img');
-let giftImage7 = document.querySelector('.giftImage7 img');
-let giftImage8 = document.querySelector('.giftImage8 img');
-let giftImage9 = document.querySelector('.giftImage9 img');
+let giftImage1 = document.getElementsByTagName('.giftImage1 img');
+let giftImage2 = document.getElementsByTagName('.giftImage2 img');
+let giftImage3 = document.getElementsByTagName('.giftImage3 img');
+let giftImage4 = document.getElementsByTagName('.giftImage4 img');
+let giftImage5 = document.getElementsByTagName('.giftImage5 img');
+let giftImage6 = document.getElementsByTagName('.giftImage6 img');
+let giftImage7 = document.getElementsByTagName('.giftImage7 img');
+let giftImage8 = document.getElementsByTagName('.giftImage8 img');
+let giftImage9 = document.getElementsByTagName('.giftImage9 img');
 
 
-// document querySelectors
+// document getElementsByTagName
 // create array
+let allHandsArray = [];
+pic1.src = allHandsArray[pic1].src;
+pic2.src = allHandsArray[pic2].src;
+pic3.src = allHandsArray[pic3].src;
+pic4.src = allHandsArray[pic4].src;
+pic5.src = allHandsArray[pic5].src;
+pic6.src = allHandsArray[pic6].src;
+pic7.src = allHandsArray[pic7].src;
+pic8.src = allHandsArray[pic8].src;
+pic9.src = allHandsArray[pic9].src;
 
 // function for handling click events
+function handleProductClick(event) {
+    console.log('click event happening' , event);
+    if(event.target === productContainer){
+      alert('please click on a product');
+    }
 // what is available when hovering on image?
-// determine if button functionality will work?
-
+// idea to toggle image on/off
+// var list = document.querySelectorAll("li");
+// for(var i = 0; i < list.length; i++) {
+//    list[i].addEventListener("click", function(e){
+//     e.currentTarget.classList.toggle("done");
+//    })
+//  }
 // function to render results - chart
 // local storage to save clicks and add number to cart, persist between pages
