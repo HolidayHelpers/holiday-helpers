4f3d91bb66c734c243ed415855dfb458f2eed822
Beth Rissman <beth.rissman@telepham.com>
2022-12-13 19:24:04 -0600

    added comments to gift and hand js files

--- js/gift.js
+++ js/gift.js
@@ -2,10 +2,35 @@
 console.log ('Hello from the gift js file');
 
 
-//we need our gift images here, with an event listener to 
-// respond to a click of the Image.
+//we need our gift giftImages here, with an event listener to
+// respond to a click of the giftImage.
 //(This is similar to what we did in the Odd Duck Project)
 
 // We need a count tally on the <section class="navCartContainer">
 // divs Gifts and Acts.
 //(This is similar to the count down button on Eva's Odd Duck, but instead, it is counting up)
+
+// define variable
+
+let giftClicks = 0;
+
+let giftImage1 = document.querySelector('.giftImage1 img');
+let giftImage2 = document.querySelector('.giftImage2 img');
+let giftImage3 = document.querySelector('.giftImage3 img');
+let giftImage4 = document.querySelector('.giftImage4 img');
+let giftImage5 = document.querySelector('.giftImage5 img');
+let giftImage6 = document.querySelector('.giftImage6 img');
+let giftImage7 = document.querySelector('.giftImage7 img');
+let giftImage8 = document.querySelector('.giftImage8 img');
+let giftImage9 = document.querySelector('.giftImage9 img');
+
+
+// document querySelectors
+// create array
+
+// function for handling click events
+// what is available when hovering on image?
+// determine if button functionality will work?
+
+// function to render results - chart
+// local storage to save clicks and add number to cart, persist between pages
--- js/hand.js
+++ js/hand.js
@@ -7,5 +7,30 @@ console.log ('Hello from the hand js file');
 //This is similar to what we did in the Odd Duck Project
 
 // We need a count tally on the <section class="navCartContainer">
-// divs Gifts and Acts.
+// divs hands and Acts.
 //(This is similar to the count down button on Eva's Odd Duck, but instead, it is counting up)
+
+// define variable
+
+let handClicks = 0;
+
+let handImage1 = document.querySelector('.handImage1 img');
+let handImage2 = document.querySelector('.handImage2 img');
+let handImage3 = document.querySelector('.handImage3 img');
+let handImage4 = document.querySelector('.handImage4 img');
+let handImage5 = document.querySelector('.handImage5 img');
+let handImage6 = document.querySelector('.handImage6 img');
+let handImage7 = document.querySelector('.handImage7 img');
+let handImage8 = document.querySelector('.handImage8 img');
+let handImage9 = document.querySelector('.handImage9 img');
+
+
+// document querySelectors
+// create array
+
+// function for handling click events
+// what is available when hovering on image?
+// determine if button functionality will work?
+
+// function to render results - chart
+// local storage to save clicks and add number to cart, persist between pages
