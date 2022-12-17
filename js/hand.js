c5f25495fca8663574c0e49e5a5206a4edd9ebce
EvaGraceSmith <smithevagrace@gmail.com>
2022-12-16 19:39:27 -0600

    worked on hover feature

--- js/hand.js
+++ js/hand.js
@@ -48,6 +48,7 @@ function renderHands(){
   for (let i=0; i < allHandArray.length; i++){
 
     let htmlDiv = document.createElement('div');
+    // htmlDiv.className='tooltip-wrap';
 
     let htmlImage = document.createElement('img');
     htmlImage.className='show';
